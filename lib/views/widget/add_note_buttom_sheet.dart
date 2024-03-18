d678b5f41c0d52bfe060124ef54f5c52f015ff4b
CoderHasnaoui <hasnouiabdilah30@gmail.com>
2024-03-18 13:34:33 +0000

    addin custom  buttom shett

--- lib/views/widget/add_note_buttom_sheet.dart
+++ lib/views/widget/add_note_buttom_sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/constants.dart';
 
 import 'custom_text_field.dart';
 
@@ -7,13 +8,31 @@ class AddButtomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding:  EdgeInsets.symmetric(horizontal: 16),
-      child:   Column(
-        children: [
-          SizedBox(height: 36,) , 
-          CustomTextField()
-        ],
+    return const  Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 36,
+            ),
+            CustomTextField(hint: 'titlle'),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: 'titlle',
+              maxLine: 5,
+            ),
+              SizedBox(
+              height: 16,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/views/widget/custom_text_field.dart
+++ lib/views/widget/custom_text_field.dart
@@ -2,15 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:note_app/views/constants.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
+  const CustomTextField({super.key , required this.hint ,  this.maxLine = 1 });
+  final String hint;
+  final int maxLine; 
   @override
   Widget build(BuildContext context) {
     return   TextField(
       
       cursorColor: KPrimaryColor,
 decoration: InputDecoration(
-  hintText: 'tittle ',
+  hintMaxLines: maxLine,
+  hintText: hint,
   hintStyle:const  TextStyle(color: KPrimaryColor),
   border:  buildBorder() ,
   enabledBorder: buildBorder() , 
