0a018f8568299ff9c0b65777e747a5b1a7a15453
CoderHasnaoui <hasnouiabdilah30@gmail.com>
2024-03-19 16:51:34 +0000

    edit note vew i done

--- lib/views/widget/edit_note_view_body.dart
+++ lib/views/widget/edit_note_view_body.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/widget/custm_appBar.dart';
+import 'package:note_app/views/widget/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();   
+    return const  Scaffold(
+     body:  Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.0),
+      child:  Column(
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomAppBar(title: 'Edit notes' , icon: Icons.check),
+          CustomTextField(hint: 'Title') , 
+          SizedBox(height: 16,) , 
+          CustomTextField(hint: 'Content ' , maxLine: 5) , 
+         
+        ],
+      ),
+    )
+    );   
   }
 }
